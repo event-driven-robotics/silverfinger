49e8cd1624928619bab8fda92d1dc3e4b8924cd7
simbamford <simbamford@gmail.com>
2021-07-21 02:55:17 +0200

    capNetwork

---
+++ arduino/capNetwork/capNetwork.ino
@@ -0,0 +1,57 @@
+/* Each of analog pins A0-A11 is connected to one metal plate of the silverfinger.
+ *  We come into the algorithm with all plates set to MODE = OUTPUT = 0
+ *  One at a time the pins are read. Each read consists of 11 cycles, one for each of the other plates.
+ *  Firstly, switch the main pin to INPUT and do an analog read from it (discard this read). This should set the mux to accept it.
+ *  For each other plate:
+ *  (b) set main plate to INPUT
+ *  (c) switch second plate to OUT = HIGH
+ *  (d) read main plate
+ *  (e) set the main plate back to OUTPUT = LOW, and set the second plate bac to OUTPUT = LOW
+ */
+
+static const uint8_t analog_pins[] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10;A11};
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  analogReadResolution(12);
+  // Setup pins for input
+  for (int i = 0; i < 12; i++) {
+    pinMode(analog_pins[i], OUTPUT);
+    digitalWrite(analog_pins[i], 0);
+  }
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Starting ...");
+}
+
+
+// the loop function runs over and over again forever
+void loop() {
+  for (int receiver = 0; receiver < 12; receiver++) {
+    // Firstly, switch the main pin to INPUT and do an analog read from it (discard this read). This should set the mux to accept it.
+    pinMode(analog_pins[receiver], INPUT);
+    int value;
+    value = analogRead(analog_pins[receiver]); // discard this read
+    for (int sender = 0; sender < 12; sender++) {
+      if (sender !=receiver) {
+        pinMode(analog_pins[receiver], INPUT);
+        pinMode(analog_pins[sender], OUTPUT);
+        digitalWrite(analog_pins[sender], 1);
+        value = analogRead(analog_pins[receiver]); 
+        Serial.print("[");
+        Serial.print(millis());
+        Serial.print(", ");
+        Serial.print(sender);
+        Serial.print(", ");
+        Serial.print(receiver);
+        Serial.print(", ");
+        Serial.print(value);
+        Serial.println("],");
+        pinMode(analog_pins[receiver], OUTPUT);
+        digitalWrite(analog_pins[receiver], 0);
+        pinMode(analog_pins[sender], OUTPUT);
+        digitalWrite(analog_pins[sender], 0);
+      }
+    }
+  }
+}
